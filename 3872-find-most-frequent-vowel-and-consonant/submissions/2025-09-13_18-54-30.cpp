fdd96360b5b69fe58ed8c4d7aeee26ee6663d7d9
krishna032003 <krishna032003@gmail.com>
2025-09-13 18:54:30 +0530

    Add solution for Find Most Frequent Vowel and Consonant (cpp)

---
+++ 3872-find-most-frequent-vowel-and-consonant/submissions/2025-09-13_18-54-30.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxFreqSum(string s) {
+        map<char,int> v,con ;
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            if(isVowel(s[i]))
+                v[s[i]]++;
+            else
+                con[s[i]]++;
+        
+        }
+        return (getmax(v)+getmax(con)); 
+    
+
+
+    }
+    static bool comp(const pair<char,int> &v1,const pair<char,int> &v2){
+        return v1.second<v2.second;
+    }
+    int getmax(const map<char,int> &v1){
+        if(v1.empty())
+        return 0;
+        auto it = max_element(v1.begin(),v1.end(),comp);
+        return it->second;
+    }
+    bool isVowel(char c){
+        switch(c){
+        case 'a' : case 'e' : case 'i' : case 'o' : case 'u' :
+            return true;
+        default: return false;
+        }
+    }
+};

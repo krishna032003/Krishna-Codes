70291793df2869782008c3c86ab3f46e30a28df3
krishna032003 <krishna032003@gmail.com>
2026-01-21 16:57:36 +0530

    Add solution for Maximum Subarray (cpp)

---
+++ 0053-maximum-subarray/latest.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0){
+            return 0;
+        }
+        vector<int> maxending(n);
+        maxending[0]=nums[0];
+        for(int i=1;i<n;i++){
+            maxending[i]=max(maxending[i-1]+nums[i],nums[i]);
+        }
+        return *max_element(maxending.begin(),maxending.end());
+    }
+};

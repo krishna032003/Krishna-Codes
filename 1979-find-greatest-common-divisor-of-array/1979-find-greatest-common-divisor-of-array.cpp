a089c38ad44ecbd906f95ec29d68ad8a12455407
Krishna Sahu <145609687+krishna032003@users.noreply.github.com>
2026-07-18 23:15:04 +0530

    Time: 0 ms (100%), Space: 16.2 MB (67.89%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findGCD(vector<int>& nums) {
+        int mx=*max_element(nums.begin(),nums.end());
+        int mn=*min_element(nums.begin(),nums.end());
+        return gcd(mx,mn);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

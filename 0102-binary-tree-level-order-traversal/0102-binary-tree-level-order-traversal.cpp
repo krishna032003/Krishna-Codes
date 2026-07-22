15fe5e7b2a21e72d1489ddd928a95942fa348714
Krishna Sahu <145609687+krishna032003@users.noreply.github.com>
2026-07-22 17:57:10 +0530

    Time: 0 ms (100%), Space: 17 MB (90.86%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root==nullptr)
+        return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int sz=q.size();
+            vector<int> level;
+            while(sz--){
+                TreeNode* curr=q.front();
+                q.pop();
+                level.push_back(curr->val);
+                if(curr->left)
+                    q.push(curr->left);
+                
+                
+                if(curr->right)
+                    q.push(curr->right);
+            }
+            ans.push_back(level);
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

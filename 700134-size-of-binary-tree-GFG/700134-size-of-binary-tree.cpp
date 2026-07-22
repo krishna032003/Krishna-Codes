6b2079a4dd2f331ea3ba8361f5b99150c449c4b9
Krishna Sahu <145609687+krishna032003@users.noreply.github.com>
2026-07-22 18:43:06 +0530

    Added solution - Time: O(N), Space: O(N)

---
+++ 700134-size-of-binary-tree-GFG/700134-size-of-binary-tree.cpp
@@ -0,0 +1,25 @@
+/* Node Structure
+class Node {
+public:
+    int data;
+    struct Node* left;
+    struct Node* right;
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+}; */
+
+class Solution {
+  public:
+    int getSize(Node* root) {
+        // code here
+        if(root==nullptr)
+        return 0;
+        return 1+getSize(root->left)+getSize(root->right);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

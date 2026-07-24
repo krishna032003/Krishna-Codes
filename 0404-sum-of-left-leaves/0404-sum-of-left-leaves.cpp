8f9a1c7333d9637ef990ebb839bfa8a2b6a01ad1
Krishna Sahu <145609687+krishna032003@users.noreply.github.com>
2026-07-24 23:31:26 +0530

    Time: 0 ms (100%), Space: 16.2 MB (58.63%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if(root==nullptr)
+        return 0;
+        int ans=0;
+        if(root->left && !root->left->left && !root->left->right )
+        ans+=root->left->val;
+        ans+=sumOfLeftLeaves(root->left);
+        ans+=sumOfLeftLeaves(root->right);
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

3f91907201e72aa92df7fa7dc4c1732ccd7ea6d7
Krishna Sahu <145609687+krishna032003@users.noreply.github.com>
2026-07-25 17:40:14 +0530

    Added solution - Time: O(N), Space: O(N)

---
+++ 700174-left-view-of-binary-tree-GFG/700174-left-view-of-binary-tree.cpp
@@ -0,0 +1,47 @@
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    vector<int> leftView(Node *root) {
+        // code here
+        if(!root)
+        return {};
+        queue<Node*> q;
+        q.push(root);
+        vector<int> ans;
+        while(!q.empty()){
+            int sz=q.size();
+            for(int i=0;i<sz;i++){
+                Node* curr=q.front();
+                q.pop();
+                if(i==0)
+                ans.push_back(curr->data);
+                if(curr->left!=nullptr)
+                q.push(curr->left);
+                
+                if(curr->right!=nullptr)
+                q.push(curr->right);
+                
+
+            }
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

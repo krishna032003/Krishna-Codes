682e3c59cb04de5c3ecf7d79f557b757f233d810
Krishna Sahu <145609687+krishna032003@users.noreply.github.com>
2026-08-17 01:24:51 +0530

    Added solution - Time: O(n log n), Space: O(log n)

---
+++ 700167-heap-sort-GFG/700167-heap-sort.cpp
@@ -0,0 +1,36 @@
+// The functions should be written in a way that array become sorted
+// in increasing order when heapSort() is called.
+
+class Solution {
+  public:
+    // Function to sort an array using Heap Sort.
+    void heapify(vector<int> &arr, int n, int i){
+        int l=2*i+1;
+        int r=2*i+2;
+        int largest=i;
+        if(l<n && arr[l]>arr[largest])
+        largest=l;
+        if(r<n && arr[r]>arr[largest])
+        largest=r;
+        if(largest!=i)
+        {
+            swap(arr[i],arr[largest]);
+            heapify(arr,n,largest);
+        }
+    }
+    void heapSort(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        for(int i=(n-2)/2;i>=0;i--){
+            heapify(arr,n,i);
+        }
+        for(int i=n-1;i>0;i--){
+            swap(arr[0],arr[i]);
+            heapify(arr,i,0);
+        }
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

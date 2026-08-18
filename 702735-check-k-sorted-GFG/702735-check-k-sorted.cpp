7c0e4355c33748d7677237194f4493ab3d7f14ee
Krishna Sahu <145609687+krishna032003@users.noreply.github.com>
2026-08-18 17:32:46 +0530

    Added solution - Time: O(N log N), Space: O(N)

---
+++ 702735-check-k-sorted-GFG/702735-check-k-sorted.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isKSortedArray(vector<int>& arr, int k) {
+        priority_queue<pair<int,int>,
+                       vector<pair<int,int>>,
+                       greater<pair<int,int>>> pq;
+
+        for(int i = 0; i < arr.size(); i++)
+            pq.push({arr[i], i});
+
+        int target = 0;
+
+        while(!pq.empty()) {
+            int original = pq.top().second;
+            pq.pop();
+
+            if(abs(original - target) > k)
+                return false;
+
+            target++;
+        }
+
+        return true;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

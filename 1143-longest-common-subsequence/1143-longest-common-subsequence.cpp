d65b0336e57a5bc897542af382e68668cb1e7624
Krishna Sahu <145609687+krishna032003@users.noreply.github.com>
2026-09-05 20:27:46 +0530

    Time: 44 ms (19.36%), Space: 27.8 MB (6.5%) - LeetHub - Time: O(mn), Space: O(mn)

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lcs(const string &s1,const string &s2,int m,int n,vector<vector<int>> &memo){
+        if(memo[m][n]!=-1)
+        return memo[m][n];
+        if(n==0 || m==0)
+        memo[m][n]=0;
+        else{
+            if(s1[m-1]==s2[n-1])
+            memo[m][n]=1+lcs(s1,s2,m-1,n-1,memo);
+            else
+            memo[m][n]=max(lcs(s1,s2,m-1,n,memo),lcs(s1,s2,m,n-1,memo));
+        }
+        return memo[m][n];
+    }
+    int longestCommonSubsequence(string s1, string s2) {
+        int m=s1.length(), n=s2.length();
+        vector<vector<int>> memo(m+1,vector<int>(n+1,-1));
+        return lcs(s1,s2,m,n,memo);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

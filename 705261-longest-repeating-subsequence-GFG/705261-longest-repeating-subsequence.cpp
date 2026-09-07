841a716d33e113da26f96b0288f58d4d99c17a56
Krishna Sahu <145609687+krishna032003@users.noreply.github.com>
2026-09-08 04:11:59 +0530

    Added solution - Time: O(n^2), Space: O(n^2)

---
+++ 705261-longest-repeating-subsequence-GFG/705261-longest-repeating-subsequence.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int longestRepSubseq(string &s) {
+        // Code here
+        int n=s.length();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1]==s[j-1] && i!=j)
+                dp[i][j]=1+dp[i-1][j-1];
+                else
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][n];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

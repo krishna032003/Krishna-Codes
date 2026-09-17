79e2aafbe008a234ef8530b4dfe0e2c437cb97dc
Krishna Sahu <145609687+krishna032003@users.noreply.github.com>
2026-09-18 03:05:35 +0530

    Time: 0 ms (100%), Space: 14.3 MB (71.01%) - LeetHub - Time: O(n log n), Space: O(n)

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size(),len=1;
+        vector<int> tail(n);
+        tail[0]=nums[0];
+        for(int i=1;i<n;i++){
+            if(nums[i]>tail[len-1]){
+            tail[len++]=nums[i];
+            }
+            else{
+                int c=lower_bound(tail.begin(),tail.begin()+len,nums[i])-tail.begin();
+                tail[c]=nums[i];
+            }
+        }
+        return len;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
